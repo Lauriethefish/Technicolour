5367f54f3d8db7c1bbcc61227f4546a5131d1174
Lauriethefish <lauriethefish@outlook.com>
2021-12-21 09:05:28 +0000

    Fix copy path

--- copy.ps1
+++ copy.ps1
@@ -1,6 +1,6 @@
 & $PSScriptRoot/build.ps1
 if ($?) {
-    adb push libs/arm64-v8a/libtechnicolor.so /sdcard/Android/data/com.beatgames.beatsaber/files/mods/libtechnicolor.so
+    adb push build/libtechnicolour.so /sdcard/Android/data/com.beatgames.beatsaber/files/mods/libtechnicolor.so
     if ($?) {
         & $PSScriptRoot/restart-game.ps1
         if ($args[0] -eq "--log") {
