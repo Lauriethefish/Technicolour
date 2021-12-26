646abe903819733a4d3c067c81d0220c4aad9386
rxzz0 <79302336+rxzz0@users.noreply.github.com>
2021-12-27 00:46:14 +0100

    Update GradientControllerUpdate.hpp

--- include/GradientControllerUpdate.hpp
+++ include/GradientControllerUpdate.hpp
@@ -4,7 +4,7 @@
 #include "UnityEngine/MonoBehaviour.hpp"
 #include "GradientController.hpp"
 
-DECLARE_CLASS_CODEGEN(Technicolour, GradientControllerUpdate, UnityEngine::MonoBehaviour,
+DECLARE_CLASS_CODEGEN(Technicolor, GradientControllerUpdate, UnityEngine::MonoBehaviour,
     DECLARE_INSTANCE_METHOD(void, Update);
 public:
     GradientController gradientController;
